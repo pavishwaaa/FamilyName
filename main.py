30219222adcaf7b37ed00a155b126c54e599ad06
pavishwaaa <pavishwa80@gmail.com>
2023-12-10 17:24:57 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#Function to Print my name
+def my_name():
+    # Use a breakpoint in the code line below to debug your script.
+    print("Vishwa Patel")  # Press Ctrl+F8 to toggle the breakpoint.
+
+my_name()
+
+#Function to input first name and print full name (family name)
+def add_family_name():
+    input_name = input("Enter your name: ")
+    print("Your full name is:", input_name, " Patel")
+
+add_family_name()
